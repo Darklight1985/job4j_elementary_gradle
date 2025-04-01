608922dcaf5fb36f69764bfc4919ab6272192c48
Darklight1985 <darklight1985@mail.ru>
2025-04-01 22:32:54 +0300

    Добавил задачу архивации JavaDoc

--- app/src/main/java/job4j_elementary_gradle/Main.java
+++ app/src/main/java/job4j_elementary_gradle/Main.java
@@ -1,6 +1,15 @@
 package job4j_elementary_gradle;
 
+/**
+ * Основной класс для запуска приложения
+ *
+ */
 public class Main {
+    /**
+     * Метод запускающий приложений
+     *
+     * @param args Массив аргументов передаваемых при запуске
+     */
     public static void main(String[] args) {
         System.out.println("Hello world");
     }
